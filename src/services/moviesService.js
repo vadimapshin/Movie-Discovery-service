f6475b23a3f7412e5d7e6bb6778228039b9189f8
Vadim Apshin <apshin97@mail.ru>
2026-04-15 19:41:19 +0300

    feat: Добавил таблицу

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -5,8 +5,11 @@ export async function getMovie(id) {
   const cached = await getMovieById(id);
 
   if (cached) {
+    console.log('CACHE HIT');
     return cached;
   }
+
+  console.log('CACHE MISS');
 
   const movie = await getMoviesDetails(id);
 
